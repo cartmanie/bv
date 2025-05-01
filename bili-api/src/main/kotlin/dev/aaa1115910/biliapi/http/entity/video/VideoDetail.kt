943ec433d119ee6e05b51cb742b050dbee01147a
Chao Jiang <cartmanie@gmail.com>
2025-05-01 20:34:17 +0800

    fix running error

--- bili-api/src/main/kotlin/dev/aaa1115910/biliapi/http/entity/video/VideoDetail.kt
+++ bili-api/src/main/kotlin/dev/aaa1115910/biliapi/http/entity/video/VideoDetail.kt
@@ -31,7 +31,7 @@ data class VideoDetail(
     @SerialName("query_tags")
     val queryTags: JsonObject? = null,
     @SerialName("is_old_user")
-    val isOldUser: Boolean
+    val is_old_user: Boolean = false
 ) {
     @Serializable
     data class HotShare(
